bb5753900675f2c8c2b4ae6fb788e9bcbefa5fd9
KoteTheInnkeeper <lucianocorrea58@gmail.com>
2021-07-30 07:26:11 -0300

    Top bar ready

--- gui/windows/main_window/ui_main_window.py
+++ gui/windows/main_window/ui_main_window.py
@@ -1,10 +1,11 @@
 import logging
 
+from gui.colors import Color
 from qt_core import *
 
 log = logging.getLogger("inventory_qt.ui_main_window")
 
-TOP_BAR_HEIGHT = 50
+TOP_BAR_HEIGHT = 30
 LEFT_MENU_WIDTH = 50
 BOTTOM_BAR_HEIGHT = 30
 
@@ -22,7 +23,7 @@ class UIMainWindow(object):
 
         # Main frame
         self.central_widget = QFrame()
-        self.central_widget.setStyleSheet("background-color: #e6e6e6;")
+        self.central_widget.setStyleSheet(f"background-color: {Color.CONTENT_BACKGROUND};")
         # A layout child of this central widget frame.
         self.main_layout = QHBoxLayout(self.central_widget)
         self.main_layout.setSpacing(0)
@@ -30,23 +31,44 @@ class UIMainWindow(object):
 
         # Left menu frame
         self.left_menu = QFrame()
-        self.left_menu.setStyleSheet("background-color: #80ffb3;")
+        self.left_menu.setStyleSheet(f"background-color: {Color.LEFT_MENU};")
         self.left_menu.setMaximumWidth(LEFT_MENU_WIDTH)
         self.left_menu.setMinimumWidth(LEFT_MENU_WIDTH)
 
         # Content frame
         self.content = QFrame()
-        self.content.setStyleSheet("background-color: #e6e6e6;")
+        self.content.setStyleSheet(f"background-color: {Color.CONTENT_BACKGROUND};")
 
         # Content layout
         self.content_layout = QVBoxLayout(self.content)
         self.content_layout.setSpacing(0)
         self.content_layout.setContentsMargins(0, 0, 0, 0)
 
-
         # Top bar frame
         self.top_bar = QFrame()
-        self.top_bar.setStyleSheet("background-color: #d7eef4; color: #005544; font: 75 25pt 'Segoe UI';")
+        self.top_bar.setMinimumHeight(TOP_BAR_HEIGHT)
+        self.top_bar.setMaximumHeight(TOP_BAR_HEIGHT)
+        self.top_bar.setStyleSheet(f"background-color: {Color.TOP_BAR}; color: #162d50;")
+        
+        # A top bar layout to organize the labels
+        self.top_layout = QHBoxLayout(self.top_bar)
+        self.top_layout.setContentsMargins(10, 0, 10, 0)
+        self.top_layout.setSpacing(0)
+        
+        # Labels for this top bar
+        self.top_label_left = QLabel("Home")
+        self.top_label_left.setStyleSheet("font: 75 18pt 'Segoe UI';")
+        self.top_label_right = QLabel("| Inventory Project")
+        self.top_label_right.setStyleSheet("font: 9pt 'Segoe UI'; text-align: right;")
+
+        # Spacer to put between them
+        self.top_spacer = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        # Adding the spacer and labesl to this top bar
+        self.top_layout.addWidget(self.top_label_left)
+        self.top_layout.addItem(self.top_spacer)
+        self.top_layout.addWidget(self.top_label_right)
+        
 
         # Pages widget
         self.pages = QStackedWidget()
@@ -54,7 +76,9 @@ class UIMainWindow(object):
 
         # Bot bar frame
         self.bot_bar = QFrame()
-        self.bot_bar.setStyleSheet("background-color: #d7eef4; color: #005544; font: 75 14pt 'Segoe UI';")
+        self.bot_bar.setMinimumHeight(BOTTOM_BAR_HEIGHT)
+        self.bot_bar.setMaximumHeight(BOTTOM_BAR_HEIGHT)
+        self.bot_bar.setStyleSheet(f"background-color: {Color.BOT_BAR}; color: #d5e5ff; font: 75 14pt 'Segoe UI';")
 
         # Adding top bar, stacked widget and bot bar.
         self.content_layout.addWidget(self.top_bar)
